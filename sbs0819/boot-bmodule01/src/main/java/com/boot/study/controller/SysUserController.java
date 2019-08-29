8293947a83a0d4deb709d568b6a4ddae324626f2
huaxueyihao <837403116@qq.com>
2019-08-29 23:13:52 +0800

    module01 layui module & index.html

--- sbs0819/boot-bmodule01/src/main/java/com/boot/study/controller/SysUserController.java
+++ sbs0819/boot-bmodule01/src/main/java/com/boot/study/controller/SysUserController.java
@@ -4,6 +4,7 @@ import com.boot.study.model.SysUser;
 import com.boot.study.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,11 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
+
+    @RequestMapping({"/index"})
+    public String index(Model model){
+        return "user/index";
+    }
 
     @GetMapping("/getAllUser")
     @ResponseBody
