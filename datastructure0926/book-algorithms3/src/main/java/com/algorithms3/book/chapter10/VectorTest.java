621405791d4e9b225bf58c9492f5ad1739cb1675
huaxueyihao <837403116@qq.com>
2019-09-29 12:29:57 +0800

    Stack api

--- datastructure0926/book-algorithms3/src/main/java/com/algorithms3/book/chapter10/VectorTest.java
+++ datastructure0926/book-algorithms3/src/main/java/com/algorithms3/book/chapter10/VectorTest.java
@@ -11,10 +11,27 @@ public class VectorTest {
 
         int a = 3;
         int b = 3;
-        System.out.println(arr[a--]);// 结果：4，说明是a[3]
-        System.out.println(a); // 2
-        System.out.println(arr[--b]);// 结果：3，说明是a[2]
-        System.out.println(b); // 2
+//        System.out.println(arr[a--]);// 结果：4，说明是a[3]
+//        System.out.println(a); // 2
+//        System.out.println(arr[--b]);// 结果：3，说明是a[2]
+//        System.out.println(b); // 2
+
+
+        int initialCapacity = 100;
+
+
+//        initialCapacity = initialCapacity | (initialCapacity >>>  1);
+
+//        initialCapacity |= (initialCapacity >>>  1);
+//        System.out.println(initialCapacity);
+//        initialCapacity |= (initialCapacity >>>  2);
+//        System.out.println(initialCapacity);
+//        initialCapacity |= (initialCapacity >>>  4);
+//        System.out.println(initialCapacity);
+//        initialCapacity |= (initialCapacity >>>  8);
+//        System.out.println(initialCapacity);
+        initialCapacity |= (initialCapacity >>> 16);
+        System.out.println(initialCapacity);
 
     }
 
--- datastructure0926/book-algorithms3/src/main/resources/chapter10/index.md
+++ datastructure0926/book-algorithms3/src/main/resources/chapter10/index.md
@@ -12,7 +12,7 @@
 >2.数组S[1..n]，最多容纳n个元素，该数组有个属性S.top，指向最新插入的元素，栈中元素S[1..S.top],S[1]栈底元素，S[S.top]是栈顶元素
 ![avatar](images/01_stack_top.jpg)
 >3.试图对空栈执行弹出，称为下溢(underflow),若是S.top超过了n，则称为栈上溢(overflow)
-
+>4.入栈和出栈的执行时间都为O(1);
 ```
 // 空栈判断伪代码
 STACK-EMPTY(S) 
@@ -35,3 +35,32 @@ else S.top = S.top - 1
 ```
 
 **对列**
+
+>1.对列上的INSERT操作称为入队(ENQUEUE)，DELETE操作称为出队(DEQUEUE)
+>2.对列有对头(head)和对尾(tail),当一个元素入队时，被放置在对尾，出队，则是队头元素
+>3.数组Q[1..n]表示n-1元素的对列，对列有属性Q.head指向对头元素，Q.tail指向对尾元素
+>4.对列Q.head,Q.head+1,...,Qtail-1，位置1紧邻在位置n后面形成一个环序，Q.head=Q.tail,对列为空，Q.head=Q.tail+1,对列是满的
+>5.入队和出队的执行时间都为O(1);
+![avatar](images/02_Queue_structure.jpg)
+
+```$xslt
+
+ENQUEUE(Q.x)
+Q[Q.tail] = x
+if Q.tail == Q.length 
+    Q.tail = 1
+else
+    Q.tail = Q.tail + 1
+    
+    
+DEQUEUE(Q)
+x = Q[Q.head]
+if Q.heead == Q.length
+    Q.head = 1
+else Q.head = Q.head + 1
+return x
+
+```
+
+
+
