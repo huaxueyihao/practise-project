1ada176c63fca87648926c38b706a524853fe779
huaxueyihao <837403116@qq.com>
2019-10-25 15:40:42 +0800

    MyBatis book: chapter01

---
+++ mybatis1025/book1-mybatis-demo01/src/main/java/com/mybatis/book/study/mapper/CountryMapper.java
@@ -0,0 +1,4 @@
+package com.mybatis.book.study.mapper;
+
+public interface CountryMapper {
+}
---
+++ mybatis1025/book1-mybatis-demo01/src/test/java/com/mybatis/book/study/CountryMapperTest.java
@@ -0,0 +1,43 @@
+package com.mybatis.book.study;
+
+import com.mybatis.book.study.model.Country;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class CountryMapperTest {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<Country> countryList = sqlSession.selectList("selectAll");
+        printCountryList(countryList);
+    }
+
+    private void printCountryList(List<Country> countryList) {
+        for (Country country : countryList) {
+            System.out.printf("%-4d%4s%4s\n", country.getId(), country.getCountryname(), country.getCountrycode());
+        }
+    }
+
+}
